58c80c5df8b93d70315ae627dcef837f8eff0736
LDS <patricio.costa@multilaser.com.br>
2020-09-24 11:26:11 -0300

    testes concluidos

--- queue.js
+++ queue.js
@@ -1,5 +1,21 @@
 function createQueue() {
-
+    return {
+        elements: [],
+        size () {
+            return this.elements.length
+        },
+        add (element) {
+            this.elements.push(element)
+            return this.elements.length
+        },
+        peek () {
+            return this.elements[0]
+        },
+        dequeue () {
+            if(!this.size()) throw new Error
+            return this.elements.shift()
+        }
+    }
 }
 
 module.exports = { createQueue }
